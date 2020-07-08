32d2f5123ba03ebdac91ae924c40272dc4459996
lenaOvc <58363859+lenaOvc@users.noreply.github.com>
2020-07-08 21:23:36 +0100

    Database and server name changed

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php   //this file is database handler in includes folder
 
-    $servername = "localhost";  //because I am using XAMPP as local server the name of the variable is local server, 
-    $dBUsername = "root";                          //when using an external server, the name of that server needs to be added there
-    $dBPassword = "";
-    $dBName = "login_database"; //the database name that I created in phpMyAdmin 
+    $servername = "lden1.mysql4.gear.host";  //because I am using XAMPP as local server the name of the variable is local server, 
+    $dBUsername = "logindatabase11";                          //when using an external server, the name of that server needs to be added there
+    $dBPassword = "Uq6ftwl4?-97";
+    $dBName = "logindatabase11"; //the database name that I created in phpMyAdmin 
 
     //need to run the connection with the database
     $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
